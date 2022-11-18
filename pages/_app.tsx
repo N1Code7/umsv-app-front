a14ad3fdc383b0587c33bd8b499643aa67c1f355
N1Code7 <n1code7@outlook.fr>
2022-11-18 12:53:08 +0100

    :tada: start project && :package: install sass && :wrench: configure Next to make SPA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : <Component {...pageProps} />}
+    </div>
+  );
 }
+
+export default App;
