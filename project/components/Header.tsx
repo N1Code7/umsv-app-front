410a39ecf5638294e9a059f8fe2ea09a49ba9d9f
N1Code7 <n1code7@outlook.fr>
2022-11-22 15:50:59 +0100

    :package: :wrench: use svg as component

--- project/components/Header.tsx
+++ project/components/Header.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { imgPath } from "../../config";
 import Navigation from "./Navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="img-container">
-        <Image src={"/assets/img/logo-club.png"} width={120} height={30} alt="logo du club" />
-      </div>
+      <Image src={"/assets/img/logo-club.png"} width={120} height={30} alt="logo du club" className="img-logo"/>
+      <Image src="/assets/img/logout.svg" width={30} height={30} alt="logout icon" className="img-logout"/>
       <Navigation />
     </header>
   )
--- project/myApp.tsx
+++ project/myApp.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Test from './routes/Test'
-import About from './routes/About'
 import Tournaments from "./routes/Tournaments"
 import Registration from "./routes/Registration"
 import Results from "./routes/Results"
@@ -11,8 +9,6 @@ export default function MyApp() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/test' element={<Test />} />
-        <Route path='/about' element={<About />} /> */}
         <Route path="/" element={<Homepage />} />
         <Route path="/tournois" element={<Tournaments />} />
         <Route path='/inscription' element={<Registration />} />
