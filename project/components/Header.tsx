f49dc10a39776efd69aaf5aa87ede90779af4093
N1Code7 <n1code7@outlook.fr>
2022-11-22 18:18:37 +0100

    :iphone: add header and navigation responsive

--- project/components/Header.tsx
+++ project/components/Header.tsx
@@ -5,15 +5,14 @@ import SwitchOnOff from "./SwitchOnOff";
 const Header = () => {
   return (
     <header className="header">
-      <Image src={"/assets/img/logo-club.png"} width={120} height={30} alt="logo du club" className="img-logo"/>
       <div className="switch-container">
         <span>Admin</span>
         <SwitchOnOff />
       </div>
+      <Image src={"/assets/img/logo-club.png"} width={120} height={30} alt="logo du club" className="img-logo"/>
       <button className="btn-logout">
         <i className="fa-solid fa-right-from-bracket img-logout"></i>
       </button>
-      {/* <Navigation /> */}
     </header>
   )
 }
